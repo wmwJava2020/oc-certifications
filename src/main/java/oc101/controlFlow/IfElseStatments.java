3326457e0fc0263af4a662d643fe51fca8dbe672
AIDA <meandaida@gmail.com>
2024-03-28 14:25:11 -0500

    testing one 101

---
+++ src/main/java/oc101/controlFlow/IfElseStatments.java
@@ -0,0 +1,15 @@
+/**
+ * Created by:AIDA
+ * Date : 3/28/2024
+ * Time : 2:21 PM
+ */
+package oc101.controlFlow;
+
+public class IfElseStatments {
+    public static void main(String[] args) {
+   for(int i = 0;i < 10; i++){
+       System.out.println("WONDAFRASh");
+   }
+    }
+
+}
