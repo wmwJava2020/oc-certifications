0b8623603758fda774c46e20a38b4812b25b256c
AIDA <meandaida@gmail.com>
2024-03-29 08:55:51 -0500

    third commit

--- src/main/java/oc101/controlFlow/IfElseStatments.java
+++ src/main/java/oc101/controlFlow/IfElseStatments.java
@@ -6,10 +6,20 @@
 package oc101.controlFlow;
 
 public class IfElseStatments {
-    public static void main(String[] args) {
-   for(int i = 0;i < 10; i++){
-       System.out.println("WONDAFRASh");
-   }
+    public static void main(String[] args) throws InterruptedException {
+        int row  = 10;
+
+        for(int x = 0; x < row;x++){
+            for(int y = row - x; y > 1; y--){
+                System.out.print(" ");
+            }
+
+            for(int y = 0; y <= x;y++){
+                Thread.sleep(100);
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
 }
