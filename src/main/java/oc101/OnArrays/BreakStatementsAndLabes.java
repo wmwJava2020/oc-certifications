e259d21c59366218a4d1191bf7e450d6d27deee4
AIDA <meandaida@gmail.com>
2024-04-08 17:07:31 -0500

    On Arrays part 1

---
+++ src/main/java/oc101/OnArrays/BreakStatementsAndLabes.java
@@ -0,0 +1,45 @@
+/**
+ * Created by:Wondafrash
+ * Date : 4/8/2024
+ * Time : 1:54 PM
+ */
+package oc101.OnArrays;
+public class BreakStatementsAndLabes {
+    public static void main(String[] args) {
+
+        String[] animal = {"Bat", "Dog", "Ox", "Cat", "Sheep", "Lion", "Tiger"};
+
+        for (String animals : animal) {
+            if (animals.equals("Sheep")) { // will stop at provided string object
+                break;
+            }
+            //System.out.println(animals);
+        }
+
+        test_loop:
+        for (String animal1 : animal) {
+            if (animal1.equalsIgnoreCase("ox")) {
+                break test_loop;
+            }
+            // System.out.println("tes_loop: " + animal1);
+        }
+
+        cat : for(String cat : animal){
+            if(cat.equals("Cat")){
+                break cat;
+            }
+           // System.out.println("cat label : "+cat);
+        }
+
+        int index = 0;
+        while(index < animal.length){
+           String animal0 = animal[index];
+
+           if(animal0.equals("Cat")){
+               break;
+           }
+            System.out.println(animal0);
+           index++;
+        }
+    }
+}
---
+++ src/main/java/oc101/OnArrays/SortingArrays.java
@@ -0,0 +1,28 @@
+/**
+ * Created by:Wondafrash
+ * Date : 4/8/2024
+ * Time : 3:47 PM
+ */
+package oc101.OnArrays;
+
+import java.util.Arrays;
+
+public class SortingArrays {
+    public static void main(String[] args) {
+        String[] str = {"Orange", "Apple", "Papaya", "Banana", "Strawberry"};
+        Arrays.sort(str);
+        for (String st : str) {
+            System.out.print(st +" ");
+        }
+        System.out.println();
+        int[] arr = {4,6,8,1,3,9,7,2,5};
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length ; i++){
+            System.out.println("arr[" + i + "] = " +arr[i]);
+        }
+        String[] s1 = {"90","10","44","20","600","300","5000"};
+        Arrays.sort(s1);
+        System.out.println(Arrays.toString(s1)); // [10, 20, 300, 44, 5000, 600, 90] bc String sorted based on the first alphabet, so 300 - 3 - i before 90 - 9 -
+
+    }
+}
