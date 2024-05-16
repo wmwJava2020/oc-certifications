feb02b44c51ba40993d235e2ed1d93a90f588fe3
AIDA <meandaida@gmail.com>
2024-05-16 16:37:05 -0500

    adding exam test and practice code
    (cherry picked from commit 4a8203ede25fa1c208dae98059625703115c30c9)

---
+++ src/main/java/oracle/certificate/exam/OCA1.java
@@ -0,0 +1,32 @@
+/**
+ * Created by:Wondafrash
+ * Date : 5/16/2024
+ * Time : 6:38 AM
+ */
+package oracle.certificate.exam;
+
+public class OCA1 {
+    //private Date rob = new Date();
+    //private java.util.Date sharon = new java.util.Date();
+
+    //static String weight = "A Lot";
+    /*default*/ double ages = 5, age1 = 5;
+    //private static boolean  success = true;
+
+
+    private static int yst = 1;
+    int tmr = 2;
+
+    public String colors;
+    public void attach(){
+        //public String color;
+    }
+
+    public String color;
+    public static void main(String[] args) {
+        OCA1 oca = new OCA1();
+        int tod =3,tmr =4;
+        System.out.println(tod + oca.tmr + oca.yst);
+
+    }
+}
